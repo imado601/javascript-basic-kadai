3640ceadd776d61b3f4002af1b7897cb2530adce
imado601 <145324974+imado601@users.noreply.github.com>
2024-02-24 13:31:28 +0900

    課題提出

---
+++ kadai_014/square.js
@@ -0,0 +1,7 @@
+//与えられた引数value_aと引数value_bを乗算し、その値を出力する関数を定義
+const multiTwoArguments = (value_a, value_b) => {
+    console.log(value_a * value_b);
+}
+
+//関数を呼び出し、引数としてそれぞれに値を渡す
+multiTwoArguments(300, 600);
